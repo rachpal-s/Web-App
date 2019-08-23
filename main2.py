0483eeaa89aa886b2756ebfa8979c60205e36137
rachhpal1176@gmail.com <rachhpal1176@gmail.com>
2019-08-23 09:14:00 +0530

    added port details in main2.py

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 # Source: https://www.freecodecamp.org/news/how-to-build-a-web-application-using-flask-and-deploy-it-to-the-cloud-3551c985e492/
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -13,4 +14,5 @@ def about():
     
 if __name__ == "__main__":
     #app.run(debug=True)
-	app.run(host='0.0.0.0', port=5001)
+	# app.run(host='0.0.0.0', port=5001)
+	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
